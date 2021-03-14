1080070e2857187efb3c412f04d65ad3bce19970
devmeist3r <devmeist3r@gmail.com>
2021-03-13 21:15:58 -0300

    *

---
+++ mod09/NetflixRemake/app/src/main/java/com/devmeist3r/netflixremake/model/Movie.java
@@ -0,0 +1,14 @@
+package com.devmeist3r.netflixremake.model;
+
+public class Movie {
+
+  private int coverURL;
+
+  public int getCoverURL() {
+    return coverURL;
+  }
+
+  public void setCoverURL(int coverURL) {
+    this.coverURL = coverURL;
+  }
+}
