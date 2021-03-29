66c69ebce2494dd4b39855e60fbd789a8a35949a
devmeist3r <devmeist3r@gmail.com>
2021-03-29 00:00:56 -0300

    *

--- mod09/NetflixRemake/app/src/main/java/com/devmeist3r/netflixremake/MovieActivity.java
+++ mod09/NetflixRemake/app/src/main/java/com/devmeist3r/netflixremake/MovieActivity.java
@@ -1,21 +1,41 @@
 package com.devmeist3r.netflixremake;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.devmeist3r.netflixremake.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MovieActivity extends AppCompatActivity {
+
+  private TextView txtTitle;
+  private TextView txtDesc;
+  private TextView txtCast;
+  private RecyclerView recyclerView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_movie);
+
+    txtTitle = findViewById(R.id.text_view_title);
+    txtDesc = findViewById(R.id.text_view_desc);
+    txtCast = findViewById(R.id.text_view_cast);
+    recyclerView = findViewById(R.id.recycler_view_similar);
 
     Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
@@ -23,16 +43,72 @@ public class MovieActivity extends AppCompatActivity {
     if (getSupportActionBar() != null) {
       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
       getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back);
+      getSupportActionBar().setTitle(null);
     }
 
     LayerDrawable drawable =
         (LayerDrawable) ContextCompat.getDrawable(this, R.drawable.shadows);
 
     if (drawable != null) {
-     Drawable movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4);
+      Drawable movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4);
       drawable.setDrawableByLayerId(R.id.cover_drawble, movieCover);
       ((ImageView) findViewById(R.id.image_view_cover)).setImageDrawable(drawable);
     }
 
+    txtTitle.setText("Batman Begins");
+    txtDesc.setText(R.string.lorem);
+    txtCast.setText(getString(R.string.cast, "Teste"));
+
+    List<Movie> movies = new ArrayList<>();
+    for (int i = 0; i < 30; i++) {
+      Movie movie = new Movie();
+      movies.add(movie);
+    }
+
+    recyclerView.setAdapter(new MovieAdapter(movies));
+    recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+
   }
+
+  private static class MovieHolder extends RecyclerView.ViewHolder {
+
+    final ImageView imageViewCover;
+
+    public MovieHolder(@NonNull View itemView) {
+      super(itemView);
+      imageViewCover = itemView.findViewById(R.id.image_view_cover);
+    }
+
+  }
+
+  private class MovieAdapter extends RecyclerView.Adapter<MovieActivity.MovieHolder> {
+
+    private final List<Movie> movies;
+
+    private MovieAdapter(List<Movie> movies) {
+      this.movies = movies;
+    }
+
+    @NonNull
+    @Override
+    public MovieActivity.MovieHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+      return new MovieActivity.MovieHolder(
+          getLayoutInflater().inflate(
+              R.layout.movie_item_similar,
+              parent, false
+          )
+      );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieHolder holder, int position) {
+      Movie movie = movies.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+      return movies.size();
+    }
+  }
+
 }
